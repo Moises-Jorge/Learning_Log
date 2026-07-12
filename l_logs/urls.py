f8c348b0b20c9350a38174ead027bcd109b534ed
Moises-Jorge <moisesjorge30@gmail.com>
2026-07-12 23:26:10 +0100

    feat: creation of the topics feature, done

--- l_logs/urls.py
+++ l_logs/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, TopicsView
 
 urlpatterns = [
-	path('', IndexView.as_view(), name='index')
+	path('', IndexView.as_view(), name='index'),
+	path('topics', TopicsView.as_view(), name='topics'),
 ]
 
--- l_logs/views.py
+++ l_logs/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .models import TopicModel, AnnotationModel
 
 class IndexView(View):
 	def get(self, request):
 		return render(request, 'l_logs/index.html')
+
+class TopicsView(View):
+    def get(self, request):
+        topics = TopicModel.objects.order_by('date_added')
+        context = {
+			'topics': topics,
+		}
+        return render(request, 'l_logs/topics.html', context)
