4d6c1ba816c4795516237e6601f16a7d58efb5bc
Moises-Jorge <moisesjorge30@gmail.com>
2026-08-12 18:29:20 +0100

    feat: register function, done

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import LogoutView
+from .views import LogoutView, RegisterView
 
 urlpatterns = [
 	path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
 	path('logout', LogoutView.as_view(), name='logout'),
+	path('register', RegisterView.as_view(), name='register')
 ]
 
--- users/views.py
+++ users/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 class LogoutView(View):
     def get(self, request):
         logout(request)
         return redirect('index')
+
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        context = {
+			'form': form,
+		}
+        return render(request, 'users/register.html', context)
+
+    def post(self, request):
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            auth_user = authenticate(username=new_user.username, password=request.POST['password1'])
+            login(request, auth_user)
+            return redirect('index')
+        context = {
+			'form': form,
+		}
+        return render(request, 'users/register.html', context)
